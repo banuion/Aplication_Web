0fb656501a64596bac518916513dd917070267b7
banuion <126978995+banuion@users.noreply.github.com>
2024-04-12 00:15:56 +0300

    Update login.php

--- login.php
+++ login.php
@@ -3,30 +3,14 @@
 // Verifică dacă scriptul este accesat prin metoda POST
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     // Preia datele trimise prin formular
-    $nume = $_POST['email'] ?? 'Nedefinit'; // Folosește 'email' pentru nume
-    $email = $_POST['parola'] ?? 'Nedefinit'; // Folosește 'parola' pentru email
-
-    // Initialize cURL session
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, "https://github.com/banuion/Aplication_Web/edit/main/login.php"); // Change to your URL
-    curl_setopt($ch, CURLOPT_POST, true);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query(array('email' => $nume, 'parola' => $email)));
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_COOKIESESSION, true);
-    curl_setopt($ch, CURLOPT_COOKIEJAR, 'cookie.txt'); // Cookies are stored here
-
-    // Execute cURL session
-    $response = curl_exec($ch);
-    if (curl_errno($ch)) {
-        echo 'Curl error: ' . curl_error($ch);
-    }
-    curl_close($ch);
+    $nume = $_POST['email'] ?? 'Nedefinit'; // Operatorul ?? este folosit pentru a returna 'Nedefinit' dacă indexul 'nume' nu există
+    $email = $_POST['parola'] ?? 'Nedefinit';
 
     // Crează o linie de text cu datele primite
     $text = "Nume: " . strip_tags($nume) . ", Email: " . strip_tags($email) . PHP_EOL;
 
     // Deschide sau creează fișierul 'date.txt' pentru a adăuga textul primit
-    $file = fopen("C:\Users\BANU\Desktop\Tehnologii_WEB_proiect\date.txt", "a"); // 'a' este modul pentru append
+    $file = fopen("C:\Users\BANU\Desktop\Tehnologii_WEB_proiect\date.txt", "a"); // 'a' este modul pentru append, adaugă la sfârșitul fișierului
 
     // Verifică dacă fișierul a fost deschis cu succes
     if ($file === false) {
@@ -36,7 +20,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         fwrite($file, $text);
         fclose($file);
 
-        echo "Datele și cookie-urile au fost salvate cu succes.";
+        echo "Datele au fost salvate cu succes.";
     }
 } else {
     // Dacă scriptul nu este accesat prin metoda POST, afișează un mesaj de eroare
